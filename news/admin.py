7ddc92df9007e28d97869f90535dcd6663ac02c6
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-04-18 23:03:18 +0500

    Register models in admin panel and configure admin site

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Region, News
 
-# Register your models here.
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('pk', 'title')
+    list_display_links = ('title',)
+
+@admin.register(Region)
+class AdminRegion(admin.ModelAdmin):
+    list_display = ('pk','title')
+
+@admin.register(News)
+class AdminNews(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'created_at', 'is_published')
+    list_display_links = ('title', 'pk')
+    list_editable = ('is_published',)
+    search_fields = ('title',)
