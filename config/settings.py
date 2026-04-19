0083685d24847493b21dd33cf9cc4012f63b2d43
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-04-19 18:35:51 +0500

    xatolar tuzatildi

--- config/settings.py
+++ config/settings.py
@@ -73,6 +73,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
